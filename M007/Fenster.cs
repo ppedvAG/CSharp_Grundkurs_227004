3554f25575631957326d238956b7def2e8b9378e
ppedvLukasK <lukask@ppedv.de>
2023-04-12 10:45:05 +0200

    M007

---
+++ M007/Fenster.cs
@@ -0,0 +1,116 @@
+﻿namespace M007;
+
+internal class Fenster
+{
+	#region Variable + Get/Set
+	private double laenge;
+
+	public double GetLaenge()
+	{
+		return laenge;
+	}
+
+	/// <summary>
+	/// Setzt die Länge des Fensters auf einen neuen Wert.
+	/// </summary>
+	/// <param name="neueLaenge">Die neue Länge des Fenstern in Meter (0 bis 10)</param>
+	public void SetLaenge(double neueLaenge)
+	{
+		if (neueLaenge > 0 && neueLaenge < 10)
+			laenge = neueLaenge;
+		else
+			Console.WriteLine("Neue Länge zu klein/groß");
+	}
+	#endregion
+
+	#region Properties
+	private double breite;
+
+	public double Breite
+	{
+		get
+		{
+			return breite;
+		}
+		set
+		{
+			if (value > 0 && value < 10)
+				breite = value;
+			else
+				Console.WriteLine("Neue Breite zu klein/groß");
+		}
+	}
+
+	public double BreiteKurz
+	{
+		get => breite;
+		set
+		{
+			if (value > 0 && value < 10)
+				breite = value;
+			else
+				Console.WriteLine("Neue Breite zu klein/groß");
+		}
+	}
+
+	private FensterStatus status = FensterStatus.Geschlossen;
+
+	public FensterStatus Status
+	{
+		get => status;
+		private set => status = value; //Nur innerhalb der Klasse setzbar
+	}
+
+	public void FensterOeffnen()
+	{
+		if (Status != FensterStatus.Offen)
+			Status = FensterStatus.Offen;
+		else
+			Console.WriteLine("Fenster ist bereits geöffnet");
+	}
+
+	public double Area
+	{
+		get
+		{
+			return laenge * breite;
+		}
+	}
+
+	public double Area2
+	{
+		get => laenge * breite;
+	}
+
+	public double Area3 => laenge * breite;
+
+	public double CalcArea() => laenge * breite;
+
+	public int Scheibenanzahl { get; set; } = 2;
+	#endregion
+
+	public Fenster() { } //Leerer Konstruktor wird gelöscht wenn ein eigener Konstruktor erstellt wird
+
+	public Fenster(double laenge, double breite)
+	{
+		this.laenge = laenge;
+		this.breite = breite;
+	}
+
+	public Fenster(double laenge, double breite, int scheiben) : this(laenge, breite)
+	{
+		Scheibenanzahl = scheiben;
+	}
+
+	~Fenster()
+	{
+		Console.WriteLine($"Fenster eingesammelt: {GetHashCode()}");
+	}
+}
+
+public enum FensterStatus
+{
+	Offen,
+	Gekippt,
+	Geschlossen
+}
---
+++ M007/Program.cs
@@ -0,0 +1,85 @@
+﻿namespace M007
+{
+	internal class Program
+	{
+		static string Info { get; set; }
+
+		static void Main(string[] args)
+		{
+			#region GC
+			for (int i = 0; i < 5; i++)
+			{
+				Fenster f = new Fenster();
+				Console.WriteLine($"Fenster {i}: {f.GetHashCode()}");
+			}
+
+			GC.Collect();
+			GC.WaitForPendingFinalizers();
+			#endregion
+
+			#region Static
+			//new Console(); //Nicht möglich, da statische Klasse
+			Console.WriteLine();
+
+			//new Program().Info //Nicht möglich
+			Program.Info = "Test";
+
+			//Test(); //Nicht möglich, da Objekt benötigt
+
+			Program[] p = new Program[5];
+			for (int i = 0; i < p.Length; i++)
+				p[i] = new Program();
+			Program.Info = "Noch ein Test";
+			#endregion
+
+			#region Werte-/Referenztypen
+			//Wertetyp
+			int original = 5;
+			int x = original;
+			original = 10;
+
+			Console.WriteLine(original.GetHashCode());
+			Console.WriteLine(x.GetHashCode());
+
+			//Referenztyp
+			Fenster f3 = new Fenster();
+			Fenster f4 = f3;
+			f3.Scheibenanzahl = 100;
+
+			Console.WriteLine(f3.GetHashCode());
+			Console.WriteLine(f4.GetHashCode());
+
+			//class
+			//Referenztyp
+			//Zuweisungen haben Referenzen statt Kopien
+			//==, != vergleichen Speicheradressen statt Inhalte
+
+			//struct
+			//Wertetyp
+			//Zuweisungen erzeugen Kopien statt Referenzen
+			//==, != werden die Werte des Objekts verglichen
+			#endregion
+
+			#region Null
+			Fenster f5; //Standard: null
+			f5 = null;
+			//f5.FensterOeffnen();
+
+			if (f5 == null || f5 != null)
+			{
+
+			}
+
+			if (f5 is null || f5 is not null) //Selbiges wie == und !=
+			{
+
+			}
+			#endregion
+		}
+
+		public void Test()
+		{
+			Main(null);
+		}
+	}
+}
