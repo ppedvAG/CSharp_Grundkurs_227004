7c58362611fca21ba52ee55e988112590051a520
ppedvLukasK <lukask@ppedv.de>
2023-04-12 09:27:49 +0200

    M006

---
+++ M006/Program.cs
@@ -0,0 +1,19 @@
+﻿namespace M006;
+
+internal class Program
+{
+	static void Main(string[] args)
+	{
+		Fenster f = new Fenster();
+		f.SetLaenge(5);
+		f.Breite = 5;
+
+		//f.Status = FensterStatus.Offen;
+
+		Fenster f2 = new Fenster(5, 5, 10);
+
+		//Console -> System
+		//File -> System.IO
+		//HttpClient -> System.Net.Http
+	}
+}
