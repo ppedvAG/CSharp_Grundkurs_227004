7f9caec877fdcaba94746aa3548650139ec06e4f
ppedvLukasK <lukask@ppedv.de>
2023-04-12 11:49:45 +0200

    M008

---
+++ M008/AccessModifier.cs
@@ -0,0 +1,17 @@
+﻿namespace M008;
+
+internal class AccessModifier
+{
+	public string Name { get; set; } //Überall sichtbar, auch außerhalb vom Projekt
+
+	private int Alter { get; set; } //Nur innerhalb der Klasse sichtbar in der das Feld angelegt wurde
+
+	internal string Wohnort { get; set; } //Überall sichtbar, aber nur innerhalb vom Projekt
+
+
+	protected string Lieblingsfarbe { get; set; } //Nur innerhalb der Klasse (effektiv private) und in Unterklassen (auch außerhalb vom Projekt)
+
+	protected internal string Lieblingsnahrung { get; set; } //Überall im Projekt (internal) und in Unterklassen außerhalb vom Projekt
+
+	protected private DateTime Geburtsdatum { get; set; } //Kann nur in dieser Klasse und in Unterklassen nur im Projekt gesehen werden
+}
---
+++ M008/Program.cs
@@ -0,0 +1,83 @@
+﻿namespace M008;
+
+internal class Program
+{
+	static void Main(string[] args)
+	{
+		Mensch m = new Mensch("Max", 23);
+		m.Name = "Test"; //Property wurde vererbt
+		m.WasBinIch(); //Methode wurde vererbt
+
+		Lebewesen lw = new Lebewesen("Test");
+
+		lw.WasBinIch2();
+		m.WasBinIch2();
+
+		Katze k = new Katze("");
+		k.WasBinIch2(); //Basisimplementation wird verwendet
+
+		Console.WriteLine(lw.ToString()); //M008.Lebewesen
+		Console.WriteLine(m.ToString()); //M008.Mensch
+		Console.WriteLine(384.ToString()); //Überschrieben -> Zahl selbst (384)
+	}
+}
+
+public class Lebewesen
+{
+	public string Name { get; set; }
+
+	public Lebewesen(string name)
+	{
+		Name = name;
+	}
+
+	public void WasBinIch()
+	{
+		Console.WriteLine("Ich bin ein Lebewesen");
+	}
+
+	public virtual void WasBinIch2()
+	{
+		Console.WriteLine("Ich bin ein Lebewesen");
+	}
+}
+
+public sealed class Mensch : Lebewesen //Mensch erbt von Lebewesen
+{
+	public int Alter { get; set; }
+
+	public Mensch(string name, int alter) : base(name) //Mit base den oberen Konstruktor verketten
+	{
+		Alter = alter;
+	}
+
+	public override sealed void WasBinIch2() //sealed: Vererbung von Klassen oder Methoden verhindern
+	{
+		//base.WasBinIch2(); //Hier mit base die obere Methode aufrufen
+		Console.WriteLine("Ich bin ein Mensch");
+	}
+
+	public override sealed string ToString()
+	{
+		return base.ToString();
+	}
+}
+
+//public class Kind : Mensch
+//{
+//	public Kind(string name, int alter) : base(name, alter)
+//	{
+//	}
+
+//	public override void wasbinich2()
+//	{
+//		base.wasbinich2();
+//	}
+//}
+
+public class Katze : Lebewesen
+{
+	public Katze(string name) : base(name)
+	{
+	}
+}
